dcda481bfd65443fb7d945dde3d6bedf466d89a7
germanebarbosa <119632836+germanebarbosa@users.noreply.github.com>
2023-05-18 03:24:00 +0000

    Se intenta agregar API sin exito.

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import fondo from "../../img/Fondo.jpg";
 
@@ -12,6 +12,55 @@ const Home = () => {
 	// Update => map
 	//style={{backgroundImage: `url(${fondo})`,
 	//backgroundRepeat: "no-repeat"}}
+
+	function getTasks (){
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/germanebarbosa', {
+			method: "GET"})
+		.then(response => response.json())
+		.then((data) => console.log(data))
+		}
+
+	function sentTasks (){
+		
+		const newTasks = [
+			{ label: "Make the bed", done: false },
+			{ label: "Walk the dog", done: false },
+			{ label: "Do the replits", done: false }
+		]
+		
+		const tareas = tasks.map((item,index) =>
+		[{label : item, done : false}]
+		)
+		console.log(tareas)
+
+
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/germanebarbosa', {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(tareas.label)
+   		})
+		// .then(resp => {
+		// 	console.log(resp.ok); // will be true if the response is successfull
+		// 	console.log(resp.status); // the status code = 200 or code = 400 etc.
+		// 	console.log(resp.text()); // will try return the exact result as string
+		// 	return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+		// })
+		// .then(data => {
+		// 	//here is where your code should start after the fetch finishes
+		// 	console.log(data); //this will print on the console the exact object received from the server
+		// })
+		// .catch(error => {
+		// 	//error handling
+		// 	console.log(error);
+		// });
+	}
+
+	useEffect(() => {
+		getTasks()
+	},[tasks])
+
 	return (
 		<>
 			<div>
@@ -26,7 +75,7 @@ const Home = () => {
 									onChange={(e)=>setTask(e.target.value)}
 									onKeyDown={(e)=> {
 										if (e.key === "Enter" && !task == ''){
-											console.log(task)
+											console.log(tasks)
 											setTasks(tasks.concat(task));
 											setTask("")
 										}
@@ -34,7 +83,7 @@ const Home = () => {
 									placeholder="What do you need to do?"/>
 							</li>
 							{tasks.map((item,index) => 
-								<li className="element">
+								<li key={index} className="element">
 									{item} <i className="icon fas fa-trash float-end" 
 										onClick={() => 
 											setTasks(
