4ff6eb88da4dbade5915ea85b6374e03982606fd
germanebarbosa <119632836+germanebarbosa@users.noreply.github.com>
2023-05-20 01:10:11 +0000

    Se devuelve a la version anterior dado que la implementacion de APIs se debia realizar en otro repositorio.

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -13,54 +13,6 @@ const Home = () => {
 	//style={{backgroundImage: `url(${fondo})`,
 	//backgroundRepeat: "no-repeat"}}
 
-	function getTasks (){
-		fetch('https://assets.breatheco.de/apis/fake/todos/user/germanebarbosa', {
-			method: "GET"})
-		.then(response => response.json())
-		.then((data) => console.log(data))
-		}
-
-	function sentTasks (){
-		
-		const newTasks = [
-			{ label: "Make the bed", done: false },
-			{ label: "Walk the dog", done: false },
-			{ label: "Do the replits", done: false }
-		]
-		
-		const tareas = tasks.map((item,index) =>
-		[{label : item, done : false}]
-		)
-		console.log(tareas)
-
-
-		fetch('https://assets.breatheco.de/apis/fake/todos/user/germanebarbosa', {
-			method: "PUT",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify(tareas.label)
-   		})
-		// .then(resp => {
-		// 	console.log(resp.ok); // will be true if the response is successfull
-		// 	console.log(resp.status); // the status code = 200 or code = 400 etc.
-		// 	console.log(resp.text()); // will try return the exact result as string
-		// 	return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-		// })
-		// .then(data => {
-		// 	//here is where your code should start after the fetch finishes
-		// 	console.log(data); //this will print on the console the exact object received from the server
-		// })
-		// .catch(error => {
-		// 	//error handling
-		// 	console.log(error);
-		// });
-	}
-
-	useEffect(() => {
-		getTasks()
-	},[tasks])
-
 	return (
 		<>
 			<div>
